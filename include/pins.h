bf79b6d740c0020c32d7febaa60576d8f792a9a9
Cole Abbott <caskisail18@icloud.com>
2026-01-11 17:55:22 -0600

    got SPI to work and cleared faults but outputs were still not enbabled

--- include/pins.h
+++ include/pins.h
@@ -5,15 +5,15 @@
 
 
 // SPI Pin Definitions
-#define SPI_SDI_PIN         23    // GPIO23: SPI Slave Data In
+#define SPI_SDI_PIN         21    // GPIO23: SPI Slave Data In
 #define SPI_SDO_PIN         22    // GPIO22: SPI Slave Data Out
-#define SPI_SCK_PIN         33    // GPIO33: SPI Clock
-#define SPI_nSCS_PIN        21    // GPIO21: SPI Chip Select
+#define SPI_SCK_PIN         17    // GPIO33: SPI Clock
+#define SPI_nSCS_PIN        23    // GPIO21: SPI Chip Select
 
 // Control Pin Definitions
-#define nSLEEP_PIN          19    // GPIO19: nSLEEP control
-#define ENABLE_PIN          18    // GPIO18: ENABLE control
-#define DIR_PIN             17    // GPIO17: DIR control
+#define nSLEEP_PIN          33    // GPIO19: nSLEEP control
+#define ENABLE_PIN          25    // GPIO18: ENABLE control
+#define DIR_PIN             13    // GPIO17: DIR control
 #define STEP_PIN            16    // GPIO16: STEP control
 
-#endif // PINS_H_
+#endif // PINS_H_h
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "drv8xx2.h"
 #include "pins.h"
 
+
 #include <SPI.h>
 
 // SPI Protocol
@@ -13,13 +14,16 @@
 #define SPI_DATA_POS       0             // Position for SPI register data bits
 #define SPI_RW_BIT_MASK    0x4000        // Mask for SPI register read write indication bit
 
+
+
+
 SPIClass * vspi = NULL;
 
 static const int spiClk = 1000000;  // 1 MHz
 
 void spiCommand(SPIClass *spi, byte data) {
   //use it as you would the regular arduino SPI API
-  spi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  spi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE1));
   digitalWrite(spi->pinSS(), LOW);  //pull SS low to prep other end for transfer
   spi->transfer(data);
   digitalWrite(spi->pinSS(), HIGH);  //pull ss high to signify end of data transfer
@@ -40,28 +44,63 @@ uint16_t spi_writeRegister(SPIClass *spi, uint8_t address, uint16_t data)
 
 
   
-  spi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  spi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE1));
   digitalWrite(spi->pinSS(), LOW);  //pull SS low to prep other end for transfer
   
-  // spi->transfer((uint8_t)((reg_value>>8) & 0xFF));  // Transmit first Byte,MSB-Byte
-  // spi->transfer((uint8_t)(reg_value & 0xFF));       // Transmit Second Byte, LSB-Byte
   spi->transfer16(reg_value);
 
   digitalWrite(spi->pinSS(), HIGH);  //pull ss high to signify end of data transfer
   spi->endTransaction();
   
-
-  // if(address == SPI_CTRL2)
-  //     DetermineMicrostepping(data);
-
   return 0;
+}
+
+// This SPI function is used to read the device configurations, parameters and
+// status information for S version of device.
+// Register format |R/W|A5|A4|A3|A2|A1|A0|*|D7|D6|D5|D4|D3|D2|D1|D0|
+// Ax is address bit, Dx is data bits and R/W is read write bit.
+// For read R/W bit should be 1.  
+uint16_t spi_readRegister(SPIClass *spi, uint8_t address)
+{
+  volatile uint16_t reg_value = 0;
+
+  reg_value |= ((address << SPI_ADDRESS_POS) & SPI_ADDRESS_MASK);         // Configure register address value
+  reg_value |= SPI_RW_BIT_MASK;                                           // Set R/W bit
+
+  spi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE1));
+  digitalWrite(spi->pinSS(), LOW);  //pull SS low to prep other end for transfer
+  
+  uint16_t received = spi->transfer16(reg_value);
+  digitalWrite(spi->pinSS(), HIGH);  //pull ss high to signify end of data transfer
+  spi->endTransaction();
+
+  
+  uint8_t dataMSB = (received >> 8) & 0xFF;
+  uint8_t dataLSB = received & 0xFF;
+  
+  // check that first 2 bits are set
+  if ( (dataMSB & 0xC0) != 0xC0 ) {
+    Serial.println("SPI read error: invalid response header!");
+    Serial.print("Received MSB: 0x");
+    Serial.println(dataMSB, HEX);
+  }
+  // check fault bits of MSB
+  if (dataMSB & UVLO_MASK || dataMSB & CPUV_MASK || dataMSB & OCP_MASK || dataMSB & STL_MASK || dataMSB & OT_MASK) {
+    Serial.println("Fault detected during SPI read!");
+  }
+
+
+  reg_value = ((((dataMSB<<8) | dataLSB) & SPI_DATA_MASK)>>SPI_DATA_POS); // complete data
+  return (reg_value);
 }
 
 
 void setup() {
+  Serial.begin(115200);
+
   //initialise the SPIClass
   vspi = new SPIClass(VSPI);
-  vspi->begin(SPI_SCK_PIN, SPI_SDI_PIN, SPI_SDO_PIN, SPI_nSCS_PIN); //SCLK, MISO, MOSI, SS
+  vspi->begin(SPI_SCK_PIN, SPI_SDO_PIN, SPI_SDI_PIN, SPI_nSCS_PIN); //SCLK, MISO, MOSI, SS
 
   pinMode(nSLEEP_PIN, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
@@ -69,11 +108,76 @@ void setup() {
   pinMode(STEP_PIN, OUTPUT);
   pinMode(SPI_nSCS_PIN, OUTPUT);
 
+  digitalWrite(nSLEEP_PIN, HIGH); // Wake up the driver
+  digitalWrite(SPI_nSCS_PIN, HIGH); // Set SS high
+  // wait t_wake = 1.5 ms
+  delayMicroseconds(2000);
+  digitalWrite(ENABLE_PIN, LOW);   // disable the driver
 
 
+  // read fault register
+  uint16_t faultReg = spi_readRegister(vspi, SPI_FAULT);
+  Serial.print("Fault Register: 0x");
+  Serial.println(faultReg, HEX);
+
+
+  // read SPI_DIAG1
+  uint16_t diag1Reg = spi_readRegister(vspi, SPI_DIAG1);
+  Serial.print("DIAG1 Register: 0x");
+  Serial.println(diag1Reg, HEX);
+
+  // read SPI_DIAG2
+  uint16_t diag2Reg = spi_readRegister(vspi, SPI_DIAG2);
+  Serial.print("DIAG2 Register: 0x");
+  Serial.println(diag2Reg, HEX);  
+  
+  // read SPI_DIAG3
+  uint16_t diag3Reg = spi_readRegister(vspi, SPI_DIAG3);
+  Serial.print("DIAG3 Register: 0x");
+  Serial.println(diag3Reg, HEX);  
+
+  // read CTRL 1
+  uint16_t ctrl1Reg = spi_readRegister(vspi, SPI_CTRL1);
+  Serial.print("CTRL1 Register: 0x");
+  Serial.println(ctrl1Reg, HEX);
+
+  // read CTRL2 
+  uint16_t ctrl2Reg = spi_readRegister(vspi, SPI_CTRL2);
+  Serial.print("CTRL2 Register: 0x");
+  Serial.println(ctrl2Reg, HEX);
+
+
+  // enable the driver
+  uint16_t ctrl1 = spi_readRegister(vspi, SPI_CTRL1);
+  Serial.print("CTRL1 Register before EN_OUT set: 0x");
+  Serial.println(ctrl1, HEX);
+  ctrl1 |= EN_OUT_MASK; // set EN_OUT bit
+  spi_writeRegister(vspi, SPI_CTRL1, ctrl1);
+  
+  ctrl1 = spi_readRegister(vspi, SPI_CTRL1);
+  Serial.print("CTRL1 Register after EN_OUT set: 0x");
+  Serial.println(ctrl1, HEX);
+
+  digitalWrite(ENABLE_PIN, HIGH);   // enable the driver
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // toggle step pin
+  digitalWrite(STEP_PIN, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(STEP_PIN, LOW);
+  delayMicroseconds(500);
+  delay(100);
+  // read the fault register
+  uint16_t faultReg = spi_readRegister(vspi, SPI_FAULT);
+  // read the index register
+  Serial.print("Fault Register: 0x");
+  Serial.print(faultReg, HEX);
+
+  uint16_t indexReg = spi_readRegister(vspi, SPI_INDEX1);
+  Serial.print(" | Index1 Register: 0x");
+  Serial.println(indexReg, HEX);
 }
 
